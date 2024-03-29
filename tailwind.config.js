5ec3686b559887cd56c62ef44ad0e0542deef403
Comfort <comfort.prince.hluyo@gmail.com>
2024-03-29 21:07:32 +0200

    Completed part of the about page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,14 @@ const plugin = require('tailwindcss/plugin')
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-  	colors: {
-    	'dark-blue': '#06172b',
-		'light-blue': '#92f9f9',
-		'light-gray': '#636d89',
-		'cream': '#f4feff'		
+    extend: {
+      colors: {
+        'dark-blue': '#06172b',
+        'light-blue': '#92f9f9',
+        'light-gray': '#636d89',
+        'cream': '#f4feff'    
+      },
     },
-    extend: {},
   },
   plugins: [
     plugin(( { addVariant } ) => { addVariant('active', '&[data-status="active"]') })
